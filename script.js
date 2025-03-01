fd144b7c833e103f35da1fbdc7a834d044704cc8
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-01 14:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+	let fname = document.querySelector("[name='fname']").value.trim();
+	let lname = document.querySelector("[name='lname']").value.trim();
+	let fullName = fname + " "+lname;
 
+	if(!fname || !lname){
+		alert("Please fill all the details")
+		return;
+	}
+
+	alert(fullName)
 }
